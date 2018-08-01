056ba52c3dfa8f098492f15b50241abfd52e6d6b
HammerGarita <36253181+HammerGarita@users.noreply.github.com>
2018-08-01 16:46:23 -0500

    Update custom-gravatar.php

--- custom-gravatar.php
+++ custom-gravatar.php
@@ -9,11 +9,11 @@
 \*------------------------------------*/
 
 // Custom Gravatar in Settings > Discussion
-function html5blankgravatar ($avatar_defaults)
+function my_theme_gravatar ($avatar_defaults)
 {
     $myavatar = get_template_directory_uri() . '/img/gravatar.jpg';
     $avatar_defaults[$myavatar] = "Custom Gravatar";
     return $avatar_defaults;
 } 
 
-add_filter('avatar_defaults', 'html5blankgravatar'); // Custom Gravatar in Settings > Discussion
+add_filter('avatar_defaults', 'my_theme_gravatar'); // Custom Gravatar in Settings > Discussion
