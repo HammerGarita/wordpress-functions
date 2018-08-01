e9f8f570bdec31c529f906af7de796f21a1f6da9
HammerGarita <36253181+HammerGarita@users.noreply.github.com>
2018-08-01 17:39:37 -0500

    Create remove-trash-option.php

---
+++ remove-trash-option.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: Hammer Garita | @hammergarita
+ * Remove trash option from Admin
+ */
+/*------------------------------------*\
+	Remove Trash Option
+\*------------------------------------*/
+
+//Remove Trash Option from Admin
+function remove_trash_option( $actions )
+{
+     if( get_post_type() === 'custom_post_type' ) //indicate the type of page on which the option will be removed
+         unset( $actions['trash'] );
+      return $actions;
+}
+
+add_filter( 'page_row_actions', 'remove_trash_options', 10, 1 );
