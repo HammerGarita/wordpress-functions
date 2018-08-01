9344d69df67b9835c0e8fbb1765f3a289a956b08
HammerGarita <36253181+HammerGarita@users.noreply.github.com>
2018-08-01 17:41:50 -0500

    Update custom-admin-title-post.php

--- custom-admin-title-post.php
+++ custom-admin-title-post.php
@@ -10,7 +10,7 @@
 //Change the placeholder 'Enter title here' in post editor
 function change_title_post( $title ){
      $screen = get_current_screen();
-     if  ( 'custom_post_type' == $screen->post_type ) {
+     if  ( 'custom_post_type' == $screen->post_type ) { //change custom_post_type by the type of page you want to change the title
           $title = 'Change the title here';
      }
      return $title;
