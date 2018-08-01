08f750fe62da1bd1dcb9ef7df37f89699e6712f3
HammerGarita <36253181+HammerGarita@users.noreply.github.com>
2018-08-01 13:24:00 -0500

    Add functions

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Author: Hammer Garita | @hammergarita
+ * Custom functions, support, custom post types and more.
+ */
+
 /*------------------------------------*\
 	External Modules/Files
 \*------------------------------------*/
@@ -17,6 +22,7 @@ if (!isset($content_width))
     $content_width = 800;
 }
 
+//Add theme support (menu, thumbnails, RSS, HTML5, translate)
 if (function_exists('add_theme_support'))
 {
     // Add Menu Support
@@ -36,7 +42,7 @@ if (function_exists('add_theme_support'))
     add_theme_support( 'html5', array( 'gallery', 'caption' ) );
 
     // Localisation Support
-    load_theme_textdomain('my_theme_textdomain', get_template_directory() . '/languages');
+    load_theme_textdomain('my_theme', get_template_directory() . '/languages');
 }
 
 /*------------------------------------*\
@@ -78,7 +84,45 @@ function my_theme_header_scripts() {
         // Enqueue Scripts
         wp_enqueue_script( 'mythemescripts' );
     }
-}
+} add_action('init', 'my_theme_header_scripts'); // Add Custom Scripts to wp_head
 
+// Load Theme styles
+function my_theme_styles() {
+    // normalize-css
+    wp_register_style( 'normalize', get_template_directory_uri() . '/css/lib/normalize.css', array(), '7.0.0' );
+    wp_enqueue_style( 'normalize' ); // Enqueue it!
+    // Custom CSS
+    wp_register_style( 'script', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
+    wp_enqueue_style( 'script' ); // Enqueue it!
+} add_action('wp_enqueue_scripts', 'my_theme_styles'); // Add Theme Stylesheet
+
+// Register Theme Navigation
+function register_my_theme_menu()
+{
+    register_nav_menus(array( // Using array to specify more menus if needed
+        'header-menu' => __('Header Menu', 'my_theme'), // Main Navigation
+        //'sidebar-menu' => __('Sidebar Menu', 'my_theme'), // Sidebar Navigation
+        //'extra-menu' => __('Extra Menu', 'my_theme') // Extra Navigation if needed (duplicate as many as you need!)
+    ));
+} add_action('init', 'register_my_theme_menu'); // Add HTML5 Blank Menu
+
+// Remove the <div> surrounding the dynamic navigation to cleanup markup
+function my_wp_nav_menu_args($args = '')
+{
+    $args['container'] = false;
+    return $args;
+} add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args'); // Remove surrounding <div> from WP Navigation
+
+// Remove Injected classes, ID's and Page ID's from Navigation <li> items
+function my_css_attributes_filter($var)
+{
+    return is_array($var) ? array() : '';
+} add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1); // Remove Navigation <li> injected classes
+
+// Remove invalid rel attribute values in the categorylist
+function remove_category_rel_from_category_list($thelist)
+{
+    return str_replace('rel="category tag"', 'rel="tag"', $thelist);
+} add_filter('the_category', 'remove_category_rel_from_category_list'); // Remove invalid rel attribute
 
 
