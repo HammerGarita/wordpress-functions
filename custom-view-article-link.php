8fa0b780030bb885b309ea4423dc3db668cafd6d
HammerGarita <36253181+HammerGarita@users.noreply.github.com>
2018-08-01 16:44:20 -0500

    Create custom-view-article-link.php

---
+++ custom-view-article-link.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Author: Hammer Garita | @hammergarita
+ * Create Custom Excerpts
+ */
+ 
+/*------------------------------------*\
+	Custom View Article Link
+\*------------------------------------*/
+
+// Custom View Article link to Post
+function my_theme_view_article($more)
+{
+    global $post;
+    return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'my_theme') . '</a>';
+} 
+
+add_filter('excerpt_more', 'my_theme_view_article'); // Add 'View Article' button instead of [...] for Excerpts
