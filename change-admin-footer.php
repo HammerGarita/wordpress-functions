b24f4a60024b17590d9b1adba7e01ae4687f60cb
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-08-02 11:08:37 -0500

    Create change-admin-footer.php

---
+++ change-admin-footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: Hammer Garita | @hammergarita
+ * Change the admin footer text
+ */
+/*------------------------------------*\
+	Change Admin Footer
+\*------------------------------------*/
+
+function change_admin_footer(){
+	 echo '<span id="footer-note">Created by <a href="http://www.nombreweb.com/" target="_blank">Your name</a>.</span>';
+}
+add_filter('admin_footer_text', 'change_admin_footer');
