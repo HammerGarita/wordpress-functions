30b370e895a207692542117749fd40c866e3be5b
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-08-03 12:15:48 -0500

    Create add-user-rol.php

---
+++ Admin Functions/add-user-rol.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author: Hammer Garita | @hammergarita
+ * @link https://codex.wordpress.org/Function_Reference/add_role
+ */
+/*------------------------------------*\
+	Add new user rol
+\*------------------------------------*/
+
+$result = add_role(
+    'basic_contributor', //Role
+    __( 'Basic Contributor' ), //Display Name
+    array( //Capabilities, for more: @link https://codex.wordpress.org/Roles_and_Capabilities#Capabilities
+        'read'         => true,  // true allows this capability
+        'edit_posts'   => true,
+        'delete_posts' => false, // Use false to explicitly deny
+    )
+);
+
+//Check that the user was created (you can delete this code)
+if ( null !== $result ) {
+    echo 'Yay! New role created!';
+}
+else {
+    echo 'Oh... the basic_contributor role already exists.';
+}
