7b789924509b277e0c8c53dd348eb75829de6ac1
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-08-03 10:19:06 -0500

    Create get-custom-taxonomies.php

---
+++ get-custom-taxonomies.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author: Hammer Garita | @hammergarita
+ * @link https://developer.wordpress.org/reference/functions/get_the_category/
+ * @linl https://developer.wordpress.org/reference/functions/get_the_terms/
+ */
+/*------------------------------------*\
+	Get the terms of the taxonomy that are attached to the post.
+\*------------------------------------*/
+
+// Get Custom Taxonomies from Post
+function get_the_category($category_name){
+  $taxonomies = "";
+  $terms = get_the_terms( $post->ID, $category_name );
+  if ( $terms && ! is_wp_error( $terms ) ) :
+    $taxonomies = array();
+    foreach ( $terms as $term ) {
+        $taxonomies[] = $term->name;
+    }
+    $taxonomies = implode(", ", $taxonomies );
+  endif;
+  return $taxonomies;
+}
+
+// Call using get_the_category('taxonomie_name');
+// Replace taxonomie_name with the name of your taxonomy
