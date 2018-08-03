a25e0bb80dbbc881caa554824a3894fe813dd005
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-08-03 12:18:02 -0500

    Update and rename add-user-rol.php to add-user-role.php

--- Admin Functions/add-user-role.php
+++ Admin Functions/add-user-role.php
@@ -4,7 +4,7 @@
  * @link https://codex.wordpress.org/Function_Reference/add_role
  */
 /*------------------------------------*\
-	Add new user rol
+	Add new user role
 \*------------------------------------*/
 
 $result = add_role(
