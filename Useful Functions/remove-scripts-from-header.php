dc44d4faab1864aa10312c86479120043db9743b
Hammer Garita <36253181+HammerGarita@users.noreply.github.com>
2018-11-13 12:08:58 -0600

    Create remove-scripts-from-header.php

---
+++ Useful Functions/remove-scripts-from-header.php
@@ -0,0 +1,15 @@
+<?php
+
+// Remove scripts from head.
+function move_scripts_from_head_to_footer() {
+    remove_action( 'wp_head', 'wp_print_scripts' );
+    remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
+    remove_action( 'wp_head', 'wp_enqueue_scripts', 1 );
+ 
+    add_action( 'wp_footer', 'wp_print_scripts', 5);
+    add_action( 'wp_footer', 'wp_enqueue_scripts', 5);
+    add_action( 'wp_footer', 'wp_print_head_scripts', 5);
+}
+add_action('wp_enqueue_scripts', 'move_scripts_from_head_to_footer');
+
+?>
